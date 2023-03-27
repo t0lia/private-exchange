e8839a349a1dc65eceb2e4a9ff46df80bfbd0706
apozdniakov <t0lia@yandex.ru>
2023-03-28 00:12:20 +0300

    PEX-6 add console logging

--- src/main/java/com/apozdniakov/privateexchange/Secret.java
+++ src/main/java/com/apozdniakov/privateexchange/Secret.java
@@ -1,21 +1,4 @@
 package com.apozdniakov.privateexchange;
 
-public class Secret {
-    private String value;
-
-
-    public Secret() {
-    }
-
-    public Secret(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
+public record Secret(String value) {
 }
--- src/main/java/com/apozdniakov/privateexchange/SecretService.java
+++ src/main/java/com/apozdniakov/privateexchange/SecretService.java
@@ -1,5 +1,7 @@
 package com.apozdniakov.privateexchange;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -9,16 +11,20 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 public class SecretService {
 
-    private Map<UUID, Secret> store = new ConcurrentHashMap<>();
+    Logger logger = LoggerFactory.getLogger(SecretService.class);
+
+    private final Map<UUID, Secret> store = new ConcurrentHashMap<>();
 
     public String getSecret(String id) {
+        logger.info("retrieving secret by id={}", id);
         UUID uuid = UUID.fromString(id);
-        return store.get(uuid).getValue();
+        return store.get(uuid).value();
     }
 
     public String createSecret(Secret secret) {
         UUID uuid = UUID.randomUUID();
         store.put(uuid, secret);
+        logger.info("secret created, id={}", uuid);
         return uuid.toString();
     }
 }
